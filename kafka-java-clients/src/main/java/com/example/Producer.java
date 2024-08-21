971ece28caecffe49254b9ed2998f17bcc7914bc
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-08-21 15:46:04 +0530

    durable kafka producer

--- kafka-java-clients/src/main/java/com/example/Producer.java
+++ kafka-java-clients/src/main/java/com/example/Producer.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -18,14 +17,28 @@ public class Producer {
                 "org.apache.kafka.common.serialization.StringSerializer");
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
+        //properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
+
+
+        
+        // safe ( durability ) producer
+        // - create topic with replication factor > 1
+        // - acks=all
+        // - producer config: retries, retry.backoff.ms, delivery.timeout.ms
+        // - min.insync.replicas=2 ( topic / broker level)
+        // - unclean.leader.election.enable=false ( broker level)
+        
+        properties.put(ProducerConfig.ACKS_CONFIG, "-1"); // 0, 1,-1/all
+        properties.put(ProducerConfig.RETRIES_CONFIG, "2147483647");
+        properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
+        properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "120000");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // Send a message
-        String topic = "topic1";
+        
         
         // way-1: Synchronous
+        // String topic = "topic1";
         // String value = "Hello, Kafka!";
         // ProducerRecord<String, String> record = new ProducerRecord<>(topic, value);
         // Future<RecordMetadata> future = producer.send(record); // Asynchronous
@@ -37,14 +50,15 @@ public class Producer {
         // }
 
         // way-2: Asynchronous    
+        String topic = "topic1";
         for (int i = 0; i < 1000000; i++) {
             // 1k sized message
             String value = "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation written in Java and Scala. The project aims to provide a unified, high-throughput, low-latency platform for handling real-time data feed\n" +
                 "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation written in Java and Scala. The project aims to provide a unified, high-throughput, low-latency platform for handling real-time data feed\n" +
                 "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation written in Java and Scala. The project aims to provide a unified, high-throughput, low-latency platform for handling real-time data feed\n" +
                 "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation write";
-            String key=List.of("key1","key2","key3").get(i%3);    
-            ProducerRecord<String, String> record = new ProducerRecord<>(topic,key, value);
+            //String key=List.of("key1","key2","key3").get(i%3);    
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic,0,null,value);
             producer.send(record, (metadata, exception) -> {
                 if (exception != null) {
                     exception.printStackTrace();
@@ -52,7 +66,7 @@ public class Producer {
                     System.out.println("Record sent to partition " + metadata.partition() + " with offset " + metadata.offset());
                 }
             });
-            TimeUnit.MILLISECONDS.sleep(1);
+            TimeUnit.SECONDS.sleep(1);
         }
         producer.close();
 
