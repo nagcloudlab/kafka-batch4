73cf902ff7298c666261a78486d0259b03915460
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2024-08-21 16:58:42 +0530

    producer client

--- kafka-java-clients/src/main/java/com/example/Producer.java
+++ kafka-java-clients/src/main/java/com/example/Producer.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -18,8 +17,6 @@ public class Producer {
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
         //properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class.getName());
-
-
         
         //-------------------------------------------
         // safe ( durability ) producer
@@ -30,13 +27,36 @@ public class Producer {
         // - producer config: retries, retry.backoff.ms, delivery.timeout.ms
         // - min.insync.replicas=2 ( topic / broker level)
         // - unclean.leader.election.enable=false ( broker level)
+        // - enable.idempotency=true
         
         properties.put(ProducerConfig.ACKS_CONFIG, "-1"); // 0, 1,-1/all
         properties.put(ProducerConfig.RETRIES_CONFIG, "2147483647");
         properties.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
         properties.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "120000");
-
+        properties.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         
+        //-------------------------------------------
+        // high throughput producer
+        //-------------------------------------------
+
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, "0");
+        properties.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "none"); // producer | topic | broker
+        properties.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+        properties.put(ProducerConfig.MAX_BLOCK_MS_CONFIG, "60000");
+
+
+        //-------------------------------------------
+        // other configuration
+        //-------------------------------------------
+
+        // 
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "producer-1");
+        properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, "com.example.ProducerInterceptor");
+
+
+
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
@@ -56,7 +76,7 @@ public class Producer {
 
         // way-2: Asynchronous    
         String topic = "topic1";
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 32; i++) {
             // 1k sized message
             String value = "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation written in Java and Scala. The project aims to provide a unified, high-throughput, low-latency platform for handling real-time data feed\n" +
                 "Apache Kafka is a distributed event store and stream-processing platform. It is an open-source system developed by the Apache Software Foundation written in Java and Scala. The project aims to provide a unified, high-throughput, low-latency platform for handling real-time data feed\n" +
@@ -71,7 +91,7 @@ public class Producer {
                     System.out.println("Record sent to partition " + metadata.partition() + " with offset " + metadata.offset());
                 }
             });
-            TimeUnit.SECONDS.sleep(1);
+            // TimeUnit.SECONDS.sleep(1);
         }
         producer.close();
 
